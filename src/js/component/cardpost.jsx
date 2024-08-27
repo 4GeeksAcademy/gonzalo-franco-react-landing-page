90f70494077f628396abf9f6ffea2c4b30da6dd9
Franco Alvarez Muñoz <falvarez.munoz@hotmail.es>
2024-08-27 18:41:57 +0000

    more proptypes for cardpost

--- src/js/component/cardpost.jsx
+++ src/js/component/cardpost.jsx
@@ -4,7 +4,9 @@ import PropTypes from "prop-types";
 const CardPost = ({
     imgUrl = "https://picsum.photos/500/325",
     tittle = "Card Title",
-    description = "Random image from Picsum!"
+    description = "Random image from Picsum!",
+    btnRef = "#",
+    btnLabel = "Find Out More!"
 }) => {
     return (
         <div className="col col-12 col-sm-6 col-lg-4 col-xl-3">
@@ -15,7 +17,7 @@ const CardPost = ({
                     <p className="card-text text-center">{description}</p>
                 </div>
                 <div className="card-footer text-center">
-                    <a href="#" className="btn btn-primary">Find Out More!</a>
+                    <a href={btnRef} className="btn btn-primary">{btnLabel}</a>
                 </div>
             </div>
         </div>
@@ -26,7 +28,9 @@ CardPost.propTypes = {
     id: PropTypes.number,
     imgUrl: PropTypes.string.isRequired,
     tittle: PropTypes.string,
-    description: PropTypes.string
+    description: PropTypes.string,
+    btnRef: PropTypes.string,
+    btnLabel: PropTypes.string
 }
 
 export default CardPost;
--- src/js/component/cardsection.jsx
+++ src/js/component/cardsection.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import CardPost from "./cardpost.jsx"
 
 const posts = [
-    { id: 1, title: 'html5', imgUrl: 'https://picsum.photos/id/125/350/350', description: 'Hola Mundo 1' },
-    { id: 2, title: 'css3', imgUrl: 'https://picsum.photos/id/666/350/350', description: 'Hola Mundo 2' },
-    { id: 3, title: 'javascript', imgUrl: 'https://picsum.photos/id/777/350/350', description: 'Hola Mundo 3' },
-    { id: 4, title: 'python', imgUrl: 'https://picsum.photos/id/539/350/350', description: 'Hola Mundo 4' },
-    { id: 5, title: 'python', imgUrl: 'https://picsum.photos/id/539/350/350', description: 'Hola Mundo 4' },
+    { id: 1, title: 'html5', imgUrl: 'https://picsum.photos/id/125/500/325', description: 'Hola Mundo 1' },
+    { id: 2, title: 'css3', imgUrl: 'https://picsum.photos/id/666/500/325', description: 'Hola Mundo 2' },
+    { id: 3, title: 'javascript', imgUrl: 'https://picsum.photos/id/777/500/325', description: 'Hola Mundo 3' },
+    { id: 4, title: 'python', imgUrl: 'https://picsum.photos/id/539/500/325', description: 'Hola Mundo 4' },
+    { id: 5, title: 'python', imgUrl: 'https://picsum.photos/id/456/500/325', description: 'Hola Mundo 5' },
 ]
 const CardSection = () => {
     return (
