1c154181b605240b3a605bd120b372efd6577989
Franco Alvarez Muñoz <falvarez.munoz@hotmail.es>
2024-08-27 14:42:40 +0000

    navbar second version and home template

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import ReactDOM from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import NavBar from "./navbar.jsx";
 
 //create your first component
-function resta(a, b) {
-	return a - b;
-}
+const Home = () => {
+	return (
+		<div className="text-center">
+			<NavBar />
+		</div>
+	);
+};
 
 export default Home;
