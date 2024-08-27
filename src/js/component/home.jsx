31f7f02f520f2f4edf7ea5805218541eeb794538
Gonzalo Cortés <gonzaloigcortescortes@gmail.com>
2024-08-27 15:36:36 +0000

    1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-import ReactDOM from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import NavBar from "./navbar.jsx";
 import Cardtitle from "./Cardtitle.jsx";
+import Cardbody from "./Cardbody.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<>
+		<div>
 			<><NavBar /></>
 			<><Cardtitle /></>
-		</>
-
-
-
-
-
+			<><Cardbody/></>
+		</div>
 
 	);
 };
